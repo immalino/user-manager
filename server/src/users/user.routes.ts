306009c9cd9ef0dbad1b11309f54793649fadeb9
immalino <fazadandyakmalin0@gmail.com>
2025-06-01 15:13:02 +0700

    feat(user): Implement PUT /users/:id endpoint for updating existing users

--- server/src/users/user.routes.ts
+++ server/src/users/user.routes.ts
@@ -1,10 +1,10 @@
 import { createRoute, z } from "@hono/zod-openapi";
-import { jsonContent, jsonContentRequired } from "stoker/openapi/helpers";
+import { jsonContent, jsonContentOneOf, jsonContentRequired } from "stoker/openapi/helpers";
 import { createErrorSchema } from "stoker/openapi/schemas";
 
 import { createSuccessSchema, ErrorResponseSchema } from "../helpers/open-api";
 import { createRouter } from "../libs/create-app";
-import { createUserSchema, selectUserSchema } from "./user.schema";
+import { createUserSchema, selectUserSchema, updateUserSchema } from "./user.schema";
 import database from "../db";
 import { usersTable } from "../db/schema";
 import { HTTPException } from "hono/http-exception";
@@ -74,37 +74,78 @@ const getUserRoute = createRoute({
   },
 });
 
+const updateUserRoute = createRoute({
+  method: "put",
+  path: "/{id}",
+  request: {
+    body: jsonContentRequired(updateUserSchema, "Create user"),
+    params: z.object({
+      id: z.string().uuid(),
+    })
+  },
+  responses: {
+    201: jsonContent(
+      createSuccessSchema(selectUserSchema, "Success update user"),
+      "Updated"
+    ),
+    400: jsonContentOneOf([createErrorSchema(updateUserSchema), createErrorSchema(z.object({ id: z.string().uuid() }))], "Bad request"),
+    404: jsonContent(ErrorResponseSchema("User not found", false), "Not found"),
+    409: jsonContent(
+      ErrorResponseSchema("Email already exists", true),
+      "Conflict"
+    ),
+    500: jsonContent(
+      ErrorResponseSchema("Internal Server Error", false),
+      "Internal Server Error"
+    ),
+  },
+});
+
 export const userRouter = createRouter()
   .openapi(createUserRoute, async (c) => {
     const userData = c.req.valid("json");
     try {
-      const [newUser] = await database.insert(usersTable).values(userData).returning();
-      return c.json({
-        success: true,
-        message: "Success create user",
-        data: newUser,
-      }, 201);
+      const [newUser] = await database
+        .insert(usersTable)
+        .values(userData)
+        .returning();
+      return c.json(
+        {
+          success: true,
+          message: "Success create user",
+          data: newUser,
+        },
+        201
+      );
     } catch (error) {
-      console.log(error)
-      if (error instanceof DrizzleQueryError && error.cause?.message === 'duplicate key value violates unique constraint "users_email_unique"') {
+      console.log(error);
+      if (
+        error instanceof DrizzleQueryError &&
+        error.cause?.message ===
+          'duplicate key value violates unique constraint "users_email_unique"'
+      ) {
         throw new HTTPException(409, {
           message: "Email already exists",
           cause: { form: true },
         });
       }
-      throw new HTTPException( 500, { message: "Internal Server Error" });
+      throw new HTTPException(500, { message: "Internal Server Error" });
     }
   })
   .openapi(getUsersRoute, async (c) => {
     const { search } = c.req.valid("query");
-    const users = await database.select().from(usersTable).where( 
-      search ?  ilike(usersTable.name, `%${search}%`) : undefined
+    const users = await database
+      .select()
+      .from(usersTable)
+      .where(search ? ilike(usersTable.name, `%${search}%`) : undefined);
+    return c.json(
+      {
+        success: true,
+        message: "Success get users",
+        data: users,
+      },
+      200
     );
-    return c.json({
-      success: true,
-      message: "Success get users",
-      data: users,
-    }, 200);
   })
   .openapi(getUserRoute, async (c) => {
     const { id } = c.req.valid("param");
@@ -113,14 +154,56 @@ export const userRouter = createRouter()
       .from(usersTable)
       .where(eq(usersTable.id, id))
       .limit(1);
-    
+
     if (!user) {
       throw new HTTPException(404, { message: "User not found" });
     }
 
-    return c.json({
-      success: true,
-      message: "Success get user",
-      data: user
-    }, 200)
+    return c.json(
+      {
+        success: true,
+        message: "Success get user",
+        data: user,
+      },
+      200
+    );
+  })
+  .openapi(updateUserRoute, async (c) => {
+    const { id } = c.req.valid("param");
+    const userData = c.req.valid("json");
+
+    let updatedUser
+
+    try {
+      [updatedUser] = await database
+        .update(usersTable)
+        .set(userData)
+        .where(eq(usersTable.id, id))
+        .returning();
+    } catch (error) {
+      if (
+        error instanceof DrizzleQueryError &&
+        error.cause?.message ===
+          'duplicate key value violates unique constraint "users_email_unique"') {
+        throw new HTTPException(409, {
+          message: "Email already exists",
+          cause: { form: true },
+        });
+      }
+
+      throw new HTTPException(500, { message: "Internal Server Error" });
+    }
+
+    if (!updatedUser) {
+     throw new HTTPException(404, { message: "User not found" });
+    }
+
+    return c.json(
+      {
+        success: true,
+        message: "Success update user",
+        data: updatedUser,
+      },
+      201
+    );
   });

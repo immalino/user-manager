9525f34aa2bfc5420f591f0ba86ed06b66abcb7c
immalino <fazadandyakmalin0@gmail.com>
2025-06-01 11:56:16 +0700

    chore: Generate initial database migration with Drizzle Kit and apply it

---
+++ server/drizzle/0000_lucky_yellow_claw.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"nama" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"phone" varchar(20) NOT NULL,
+	"isActive" boolean DEFAULT true NOT NULL,
+	"department" varchar(100) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);

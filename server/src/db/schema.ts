3ef4c6e1edf6b51325d18e907248a5c4c69f6177
immalino <fazadandyakmalin0@gmail.com>
2025-06-01 11:55:11 +0700

    feat: Define user schema using Drizzle ORM

---
+++ server/src/db/schema.ts
@@ -0,0 +1,14 @@
+import { boolean, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("nama", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  phone: varchar("phone", { length: 20 }).notNull(),
+  isActive: boolean("isActive").default(true).notNull(),
+  department: varchar("department", { length: 100 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});

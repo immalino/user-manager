92fd38466b37e5ce26c41347e3d99a0a85079a6c
immalino <fazadandyakmalin0@gmail.com>
2025-06-02 17:20:10 +0700

    refactor(types): remove unused success response type and related imports

--- shared/types.ts
+++ shared/types.ts
@@ -1,16 +1,8 @@
-export type SuccessResponse<T = void> = {
-  success: true;
-  message: string;
-} & (T extends void ? object : { data: T });
-
 export interface ErrorResponse {
   success: false;
   error: string;
   isFormError?: boolean;
 }
-
-import { router } from "@/src";
-export type AppType = typeof router;
 
 export type {
   CreateUser,
@@ -19,10 +11,3 @@ export type {
   UpdateUser,
   DeleteUser,
 } from "../server/src/users/user.routes";
-
-export {
-  createUserSchema,
-  selectUserSchema,
-  updateUserSchema,
-  User,
-} from "../server/src/users/user.schema";
